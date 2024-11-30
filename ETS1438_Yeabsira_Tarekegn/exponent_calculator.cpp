f1ae92d64de19c395708a09f2797a4ce1737981a
Yeabsira_tarekegn <yeabsira.tarekegn@aastustudent.edu.et>
2024-11-29 23:16:56 -1100

    Create exponent_calculator.cpp

---
+++ ETS1438_Yeabsira_Tarekegn/exponent_calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y;
+    double result;
+    result=1.0;
+cout << "Enter the base as x: ";
+    cin >> x;
+    cout << "Enter the exponent as y : ";
+    cin >> y;
+  if (y == 0) {
+        result = 1;
+    } else if (y > 0) {
+        for (int i = 1; i <= y; i++) {
+            result *= x;
+        }
+    } else {
+        for (int i = 1; i <= -y; i++) {
+            result *= x;
+        }   result = 1.0/ result; }
+    cout << x << "^" << y << " = " << result << endl;
+  return 0;
+}
+
