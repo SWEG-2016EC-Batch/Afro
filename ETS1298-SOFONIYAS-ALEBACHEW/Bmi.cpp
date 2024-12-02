661bd2bc67a17fded223ec1789ec8cc78eed4831
Sofoniyas Alebachew <sofi123man@gmail.com>
2024-12-02 11:23:02 +0300

    Create Bmi.cpp

---
+++ ETS1298-SOFONIYAS-ALEBACHEW/Bmi.cpp
@@ -0,0 +1,54 @@
+/* Make a program that calculates the body mass index (BMI) of a person and 
+* determines weather the personis normal weight,under weight or overweight.
+*BMI is calculated as (BMI=w/h*h). Make the program work for multiple person before terminate.
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	float height, weight, bmi,num;
+
+	cout<<"PLEASE ENTER THE NUMBER OF PEOPLE YOU WOULD LIKE TO CALCULATE BMI: ";
+
+	cin>>num;
+
+	for(int i=1; i<=num; i++){
+
+	cout<<"PLEASE ENTER WEIGHT " <<i<< " IN (KG): ";
+
+	cin>>weight;
+
+	cout<<"PLEASE ENTER HEIGHT " <<i<< " IN (M): ";
+
+	cin>>height; 
+
+	bmi = weight/(height*height);
+
+	cout<<"YOUR BMI IS: "<<bmi<<endl;
+
+	if(bmi<18.5){
+
+    cout<<"You are under weight."<<endl;
+
+	}
+
+	else if(bmi>=18.5 && (bmi<=24.9)){
+
+		cout<<"You are normal weight."<<endl;
+
+		}
+
+	else{
+
+		cout<<"You are over weight."<<endl;
+
+	}
+
+	}
+	return 0;
+
+}
