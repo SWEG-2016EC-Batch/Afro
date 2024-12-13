b0cf434f2b319a663cd35646244a14bf263df816
SOLIYANA KIBROM MEHIBUB <soliyana.kibrom@aastustudent.edu.et>
2024-12-14 00:06:34 +0300

    Update net pay 2.cpp

--- ETS1303-Soliyana-Kibrom/net pay 2.cpp
+++ ETS1303-Soliyana-Kibrom/net pay 2.cpp
@@ -1,3 +1,9 @@
+/*Develop a program that find the Net-Pay of an employee after
+*deduction of pension (7%) and tax based on tax rate provided. The
+*program should read the basic salary, worked hours, and bonus
+*rate. If the employee worked hours exceed 40 hrs., the program
+*prompts the user to enter an over-time bonus rate/hour.*/
+
 #include <iostream>
 using namespace std;
 int main (){
